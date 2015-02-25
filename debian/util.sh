4508fe1e85eda0a1180d34e78fa5219aaf09c411
Ken Rice <krice@freeswitch.org>
2015-02-25 15:03:38 -0600

    add a util script for building packages with build number bump

---
+++ debian/util.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]; then
+	CHANGELOG=/tmp/changelog.tmp.$$
+
+	cat >> $CHANGELOG << EOF
+libpng1.6 (1.6.10-$1) experimental; urgency=low
+
+  * AutoBuild Revision Update
+
+ -- FreeSWITCH Autobuild <autobuild@freeswitch.org> `date -R`
+
+EOF
+	cat debian/changelog >> $CHANGELOG
+	cp $CHANGELOG debian/changelog
+	rm $CHANGELOG
+
+fi
+
+git-buildpackage --git-upstream-tree=origin/upstream -us -uc -sa --git-ignore-new
