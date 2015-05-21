62376ed989cd1198e6a790e1cad903566ba98779
Christopher Rienzo <chris.rienzo@citrix.com>
2015-05-21 13:03:17 -0400

    FS-7553 #comment add RPM packaging for libpng 1.6

---
+++ build_rpm.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+ORIGDIR=`pwd`
+cd ..
+(mkdir -p rpmbuild && cd rpmbuild && mkdir -p SOURCES BUILD BUILDROOT i386 x86_64 SPECS)
+
+cp ${ORIGDIR}/redhat/*.spec rpmbuild/SPECS/.
+cp ${ORIGDIR}/redhat/*.patch rpmbuild/SOURCES/.
+cp ${ORIGDIR}/pngusr.dfa rpmbuild/SOURCES/.
+cp ${ORIGDIR}/*.gz rpmbuild/SOURCES/.
+
+rpmbuild --define "_topdir %(pwd)/rpmbuild" \
+  --define "_rpmdir %{_topdir}" \
+  --define "_srcrpmdir %{_topdir}" \
+  -ba rpmbuild/SPECS/libpng.spec
